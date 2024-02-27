2e2613cf3578715ea6bd17981133f0f07ddac072
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-02-27 09:31:53 -0500

    init

---
+++ order/orderApp.swift
@@ -0,0 +1,17 @@
+//
+//  orderApp.swift
+//  order
+//
+//  Created by Christopher Peralta on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct orderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
